a8fbdee524dadfc1607119765fc3a09df2042ea8
Robin Betz <robin@robinbetz.com>
2016-07-11 14:35:16 -0700

    Add missing B2AR alp dataset

--- beak/msm/datasets.py
+++ beak/msm/datasets.py
@@ -21,7 +21,7 @@ def b2ar_alprenolol():
         prefix = "/scratch/PI/rondror/"
     topology = prefix + "rbetz/C.psf"
     skelly = prefix + "rbetz/pnasc/DESRES-Trajectory_pnas2011a-C-%d-no-water-no-lipid/pnas2011a-C-%d-no-water-no-lipid/pnas2011a-C-%d-no-water-no-lipid-*.dcd"
-    for i in range(9):
+    for i in range(10):
         for x,g in enumerate(sorted(glob(skelly % (i,i,i)))):
             filenames.append(g)
             identifiers.append("C%d-%d" % (i,x))
