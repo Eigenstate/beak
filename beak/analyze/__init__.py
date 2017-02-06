98c8c953f16634e59b4e20668ea8542321ee69ba
Robin Betz <robin@robinbetz.com>
2017-02-06 14:29:15 -0800

    Add notebookmd analysis functions

--- beak/analyze/__init__.py
+++ beak/analyze/__init__.py
@@ -3,6 +3,7 @@
 __version__ = '0.0.0a1'
 __author__  = 'Robin Betz'
 
+import notebookmd
 from .Analyzer import Analyzer
 from .MinDistanceAnalyzer import MinDistanceAnalyzer
 from .RmsdAnalyzer import RmsdAnalyzer
