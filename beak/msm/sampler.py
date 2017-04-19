4a530b3eec84b43f5f93d0496bc1db2a3d120cba
Robin Betz <robin@robinbetz.com>
2017-04-19 16:17:32 -0700

    Add densitysampler

--- beak/msm/sampler.py
+++ beak/msm/sampler.py
@@ -3,11 +3,13 @@ Contains functionality for loading project-specific datasets
 """
 from __future__ import print_function
 import os
-import h5py
+import numpy as np
+import random
 from configparser import RawConfigParser
 from glob import glob
 from msmbuilder.utils import load
 from socket import gethostname
+from . import utils
 #pylint: disable=import-error,no-name-in-module
 try:
     from VMD import evaltcl
@@ -121,18 +123,7 @@ class Sampler(object):
         Loads features on demand
         """
 # TODO: use utils
-        featurized = self.config["system"]["featurized"]
-        feats = []
-        for gen in range(self.firstgen, self.generation+1):
-            if os.path.isfile("%s.h5" % featurized % gen):
-                h5f = h5py.File("%s.h5" % featurized % gen, 'r')
-                for i in sorted(h5f.keys(), key=int):
-                    feats.append(h5f[i][:])
-                h5f.close()
-            else:
-                feats.extend(load("%s.pkl" % featurized % gen))
-
-        self.features = [f[::self.stride] for f in feats]
+        pass
 
     #==========================================================================
 
@@ -216,7 +207,7 @@ class Sampler(object):
             (list of int): Residue numbers of each ligands, in order
         """
         ligids = sorted(set(atomsel("resname %s" % " ".join(self.ligands),
-                            molid=molid).get("residue")))
+                                    molid=molid).get("residue")))
         return ligids
 
 #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
@@ -286,7 +277,7 @@ class ClusterSampler(object):
         if os.path.isfile(os.path.join(substr, "testing.mcluster.pkl")):
             self.mclust = load(os.path.join(substr, "testing.mcluster.pkl"))
         else:
-            self.mclust = clust
+            self.mclust = self.clust
             self.clust = None
 
         for gen in range(1, self.generation+1):
@@ -357,7 +348,7 @@ class ClusterSampler(object):
             molrep.addrep(a, style="Licorice", material="Opaque",
                           selection="noh and same fragment as "
                                     "(resname %s)" % " ".join(self.ligands),
-                                    color="User")
+                          color="User")
             molrep.set_scaleminmax(a, molrep.num(a)-1, 0, 2.)
             self.molids.append(a)
 
@@ -383,3 +374,132 @@ class ClusterSampler(object):
     #==========================================================================
 
 #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+class DensitySampler(object):
+    """
+    Loads up a bunch of densities along with random frames representing
+    each cluster
+    """
+
+    def __init__(self, configfile, **kwargs):
+        """
+        Creates a sampler explorer object.
+
+        Args:
+            configfile (str): Configuration file to read generation and
+                file info from
+            filenames (list of str): Production files
+            clustdir (str): Directory with cluster dx files
+            clusters (str): Cluster data pickle path
+            msm (str): MSM pickle path
+        """
+        # Set visualization stuff
+        evaltcl("color scale method BGR")
+        evaltcl("display projection Orthographic")
+
+        self.config = RawConfigParser()
+        self.config.read(configfile)
+
+        self.ligands = self.config["system"]["ligands"].split(',')
+        self.nligs = int(self.config["system"]["num_ligands"])
+
+        self.generation = kwargs.get("generation")
+        self.clustdir = kwargs.get("clustdir")
+        self.prodfiles = kwargs.get("prodfiles")
+
+        # Load reference structure for later alignment
+        self.reference = self.config["system"]["reference"]
+        if "prmtop" in self.reference:
+            self.refid = molecule.load("parm7", self.reference,
+                    "crdbox", self.reference.replace("prmtop", "inpcrd"))
+        elif "psf" in self.reference:
+            self.refid = molecule.load("psf", self.reference,
+                    "pdb", self.reference.replace("psf", "pdb"))
+        self.refsel = self.config["system"]["refsel"]
+        self.aselref = atomsel(self.refsel, molid=self.refid)
+        self.psfsel = self.config["system"]["canonical_sel"]
+
+        # List all of the filenames so we can look them up later
+        self.molids = {}
+        self._load_densities()
+
+        # Find and load the msm, and clusters
+        self.msm = load(kwargs.get("msm"))
+        self.clusters = load(kwargs.get("clusters"))
+
+    #==========================================================================
+
+    def __del__(self):
+        for mid in self.molids.values():
+            molecule.delete(mid)
+        del self.clusters
+        del self.msm
+
+    #==========================================================================
+
+    def _load_densities(self):
+        """
+        Loads all clusters and density map
+        """
+        for cfile in sorted(glob(os.path.join(self.clustdir, "*.dx")),
+                key=lambda x:int(x.split("/")[-1].replace(".dx",""))):
+            cid = int(cfile.split("/")[-1].replace(".dx", ""))
+            if "prmtop" in self.reference:
+                self.molids[cid] = molecule.load("parm7", self.reference, "crdbox",
+                                                 self.reference.replace("prmtop", "inpcrd"))
+            else:
+                self.molids[cid] = molecule.load("psf", self.reference, "pdb",
+                                                 self.reference.replace("psf", "pdb"))
+
+            molecule.rename(self.molids[cid], str(cid))
+            molecule.read(self.molids[cid], "dx", cfile, waitfor=-1)
+            molrep.delrep(self.molids[cid], 0)
+            molrep.addrep(self.molids[cid], style="NewRibbons 0.1 12.0 12.0",
+                          selection="protein or resname ACE NMA",
+                          color="ColorID %d" % (cid%33), material="Opaque")
+            evaltcl("mol drawframes %d %d %d"
+                    % (self.molids[cid], molrep.num(self.molids[cid])-1, 0))
+            molrep.addrep(self.molids[cid], style="Isosurface 0.05 0 0 1 1",
+                          color="ColorID %d" % (cid%33))
+
+    #==========================================================================
+
+    def _load_frame(self, cluster):
+        """
+        Picks a frame corresponding to the given cluster and loads it,
+        setting representation to the correct ligand
+        Args:
+            cluster(int): The cluster to sample
+        """
+        # Pick a frame at random containing this cluster
+        frames = {k:v for k, v in {i:np.ravel(np.where(c == cluster)) \
+                  for i, c in enumerate(self.clusters)}.items() \
+                  if len(v)}
+        clustindex = int(random.choice(frames.keys()))
+        frameindex = int(random.choice(frames[clustindex]))
+        filename = self.prodfiles[clustindex / self.nligs]
+
+        # Load this frame
+        molecule.read(self.molids[cluster],
+                      utils.get_trajectory_format(filename), filename,
+                      beg=frameindex, end=frameindex, waitfor=-1)
+
+        # Set representations so this frame appears
+        ligidx = clustindex % self.nligs
+        molrep.addrep(self.molids[cluster], style="Licorice 0.3 12.0 12.0",
+                      selection="noh and same fragment as residue %d" % ligidx,
+                      color="ColorID %d" % (cluster%33), material="Opaque")
+        evaltcl("mol drawframes %d %d %d"
+                % (self.molids[cluster], molrep.num(self.molids[cluster])-1,
+                   molecule.numframes(self.molids[cluster])))
+
+    #==========================================================================
+
+    def add_frames(self, cluster, num=10):
+        """
+        Adds num random frames depicting the given cluster to
+        the given molecule
+        """
+        for _ in range(num):
+            self._load_frame(cluster)
+
