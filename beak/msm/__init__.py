d251a6e159f5b7d320ff9f6c2f2970458dba21c9
Robin Betz <robin@robinbetz.com>
2017-09-15 14:39:28 -0700

    Remove reference to ClusterCenter

--- beak/msm/__init__.py
+++ beak/msm/__init__.py
@@ -3,7 +3,7 @@
 __version__ = '0.0.0a1'
 __author__  = 'Robin Betz'
 
-from beak.msm.aggregator import ClusterCenter, ClusterDensity
+from beak.msm.aggregator import ClusterDensity, ParallelClusterDensity
 from beak.msm.sampler import Sampler, ClusterSampler
 from beak.msm.utils import *
 from beak.msm.walker import *
