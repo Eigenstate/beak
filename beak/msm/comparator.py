349a1a96c95a1f65e81231619d721564936d1a8f
Robin Betz <robin@robinbetz.com>
2017-09-29 12:48:28 -0700

    Have comparator return list of clusters with all possible useful data aggregated therein.
    Also be less confused with MSM internal labels vs. state labels

--- beak/msm/comparator.py
+++ beak/msm/comparator.py
@@ -1,6 +1,7 @@
 """
 Contains classes for comparing clusters across multiple generations.
 """
+#pylint: disable=invalid-name, wrong-import-order
 import os
 import numpy as np
 
@@ -12,26 +13,28 @@ from vmd import molecule, vmdnumpy
 #                            Cluster comparators                              #
 #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-class ClusterEquivalent(object):
+class Cluster(object):
     """
-    Represents two equivalent clusters, and their associated stats.
+    Represents a single cluster, with associated data relevant for plotting
 
-    Attributs:
-        gen1 (int): Generation of first cluster
-        gen2 (int): Generation of second cluster
-        label1 (int): Label of first clustesr
-        label2 (int): Label of second cluster
-        mean_rmsd (float): RMSD between mean coordinates of both clusters
+    Attributes:
+        label (int): Cluster label
+        generation (int): Generation cluster came from
+        rmsd_to_bound (float): RMSD to bound state
+        hub_score (float): Hub score
+        population (float): Predicted equilibrium population
     """
-    def __init__(self, gen1, gen2, label1, label2, rmsd):
-        self.gen1 = gen1
-        self.gen2 = gen2
-        self.label1 = label1
-        self.label2 = label2
-        self.rmsd = rmsd
+    def __init__(self, **kwargs):
+        self.label = kwargs.get("label")
+        self.generation = kwargs.get("generation")
+        self.rmsd_to_bound = kwargs.get("rmsd_to_bound")
+        self.hub_score = kwargs.get("hub_score")
+        self.population = kwargs.get("population")
 
-    def __str__(self):
-        return "%d <- %d RMSD=%f" % (self.label1, self.label2, np.mean(self.rmsd))
+    #===========================================================================
+
+    def __repr__(self):
+        return "G%sc%s" % (self.generation, self.label)
 
 #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
@@ -39,17 +42,35 @@ def get_bound_state(rootdir):
     """
     Returns bound coordinates as numpy array. Only b2ar supported
     """
+    mid = None
     if "B2AR" in rootdir and "50ns" in rootdir:
         mid = molecule.load("mae", os.path.join(rootdir, "..", "bound_5.mae"))
         mask = vmdnumpy.atomselect(mid, 0, "noh and resname DALP")
+    elif "dipp" in rootdir:
+        mid = molecule.load("mae", os.path.join(rootdir, "..", "pep_bound",
+                                                "prep", "4RWD_prepped_bound.mae"))
+        mask = vmdnumpy.atomselect(mid, 0,
+                                   "noh and same fragment as resname DI7 DI78")
+
+    if mid:
         coords = np.compress(mask, vmdnumpy.timestep(mid, 0), axis=0)
         molecule.delete(mid)
         return coords
+    else:
+        return None
 
-#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#==============================================================================
+
+def rmsd_to(coords, boundcoords):
+    """
+    Gets the RMSD, coordinate wise, between 2 things
+    """
+    return np.sqrt(np.sum((coords-boundcoords)**2, axis=0))
+
+#==============================================================================
 
 def compare_clusters(gen, prevgen, rootdir,
-                     varthreshold=5.0, threshold=10.0):
+                     varthreshold=5.0, threshold=5.0, **kwargs):
     """
     Finds clusters that match up in terms of mean position.
 
@@ -61,15 +82,25 @@ def compare_clusters(gen, prevgen, rootdir,
         threshold (float): RMSD threshold factor (TODO)
 
     Returns:
-        (list of ClusterEquivalent): Matching clusters, inorder
+        (list of tuple of Clusters): Matching clusters, inorder
     """
-    means = utils.load(os.path.join(rootdir, "clusters", str(gen), "means.pkl"))
-    varxs = utils.load(os.path.join(rootdir, "clusters", str(gen),
-                                    "variance.pkl"))
-    oldmeans = utils.load(os.path.join(rootdir, "clusters", str(prevgen),
-                                       "means.pkl"))
-    oldvarxs = utils.load(os.path.join(rootdir, "clusters", str(prevgen),
-                                       "variance.pkl"))
+    d = os.path.join(rootdir, "clusters", str(gen))
+    means = kwargs.get("means", utils.load(os.path.join(d, "means.pkl")))
+    varxs = utils.load(os.path.join(d, "variance.pkl"))
+
+    d = os.path.join(rootdir, "production", str(gen))
+    msm = kwargs.get("msm",
+                     utils.load(os.path.join(d, "mmsm_G%d.pkl" % gen)))
+    scores = kwargs.get("scores",
+                        utils.load(os.path.join(d, "mmsm_scores.pkl")))
+
+    d = os.path.join(rootdir, "clusters", str(prevgen))
+    oldmeans = utils.load(os.path.join(d, "means.pkl"))
+    oldvarxs = utils.load(os.path.join(d, "variance.pkl"))
+
+    d = os.path.join(rootdir, "production", str(prevgen))
+    oldmsm = utils.load(os.path.join(d, "mmsm_G%d.pkl" % prevgen))
+    oldscores = utils.load(os.path.join(d, "mmsm_scores.pkl"))
 
     if len(varxs.keys()) != 50:
         print("MISSING KEYS: gen %d" % gen)
@@ -77,20 +108,39 @@ def compare_clusters(gen, prevgen, rootdir,
     validmeans = [m for m in means if np.mean(varxs[m]) < varthreshold]
     validoldmeans = [m for m in oldmeans if np.mean(oldvarxs[m]) < varthreshold]
 
-    # Get + load bound state
+    # Get + load bound state, solvent population
     boundcoords = get_bound_state(rootdir)
 
     connecteds = []
     for m in validmeans:
-        bestrmsd = threshold * np.ones(means[0].shape)
+        bestrmsd = threshold * np.ones(list(means.values())[0].shape)
         candidate = ()
         for mm in validoldmeans:
-            rmsd = np.sqrt(np.sum((means[m] - oldmeans[mm])**2, axis=0))
+            rmsd = rmsd_to(means[m], oldmeans[mm])
             if np.all(rmsd < bestrmsd):
-                brms = np.mean(np.sqrt(np.sum((means[m]-boundcoords)**2,
-                                              axis=0)))
-                candidate = (mm, m, brms)
+
+                l1 = oldmsm.transform([mm])[0][0]
+                l2 = msm.transform([m])[0][0]
+                c1 = Cluster(label=mm,
+                             generation=prevgen,
+                             population=oldmsm.populations_[l1],
+                             hub_score=oldscores[l1],
+                             rmsd_to_bound=np.mean(rmsd_to(oldmeans[mm],
+                                                           boundcoords))
+                                           if np.any(boundcoords)
+                                           else None)
+                c2 = Cluster(label=m,
+                             generation=gen,
+                             population=msm.populations_[l2],
+                             hub_score=scores[l2],
+                             rmsd_to_bound=np.mean(rmsd_to(means[m],
+                                                           boundcoords))
+                                           if np.any(boundcoords)
+                                           else None)
+
+                candidate = (c1, c2)
                 bestrmsd = rmsd
+
         if candidate:
             connecteds.append(candidate)
 
@@ -106,58 +156,55 @@ def plot_cluster_progress(config, mingen, maxgen):
         config (str): Path to configuration file for run
 
     Returns:
-        (fig) Plottable matplotlib figure
+        (list of lists of Clusters) : Equivalent clusters
     """
-    assert(os.path.isfile(config))
+    assert os.path.isfile(config)
     cfg = ConfigParser(interpolation=None)
     cfg.read(config)
     rootdir = cfg.get("system", "rootdir")
-
-    # Accumulate generations with a variance pickle file
-    # TODO: Actually calculate variance for older generations where
-    #       it wasn't calculated at the time
-#    gen = cfg.getint("production", "generation")
-#    ggen = (g for g in range(1, gen+1)
-#            if os.path.isfile(os.path.join(rootdir, "clusters", str(gen),
-#                                           "variance.pkl")))
+    boundcoords = get_bound_state(rootdir)
 
     # Obtain a list of clusters across generations
-    plot_data = { k : [] for k in range(mingen+1, maxgen+1) }
+    plot_data = []
     counter = 0 # DEBUG
 
-    # Initialize array with first found cluster set
-    msm = None
-    prevmsm = utils.load(os.path.join(rootdir, "production", str(mingen),
-                                      "mmsm_G%d.pkl" % mingen))
-    #plot_data[mingen] = [[pop] for pop in prevmsm.populations_]
-
-    #for g in range(mingen+1, maxgen+1):
     for g in range(mingen+1, maxgen+1):
-        msm = utils.load(os.path.join(rootdir, "production", str(g),
-                                      "mmsm_G%d.pkl" % (g)))
-        result = compare_clusters(gen=g, prevgen=g-1, rootdir=rootdir)
+        mdir = os.path.join(rootdir, "production", str(g))
+        msm = utils.load(os.path.join(mdir, "mmsm_G%d.pkl" % g))
+        scores = utils.load(os.path.join(mdir, "mmsm_scores.pkl"))
+        means = utils.load(os.path.join(rootdir, "clusters",
+                                        str(g), "means.pkl"))
+        appended = []
 
         # Aggregate and get populations
-        appended = []
+        result = compare_clusters(gen=g, prevgen=g-1, rootdir=rootdir,
+                                  msm=msm, scores=scores, means=means)
         for r in result:
-            l1 = prevmsm.inverse_transform([r[0]])[0][0]
-            l2 = msm.inverse_transform([r[1]])[0][0]
-
-            for biglist in plot_data.values():
-                for entry in biglist:
-                    if entry[-1][0] == prevmsm.populations_[l1]:
-                        entry.append((msm.populations_[l2], r[2]))
-                        appended.append(l2)
-                        counter += 1
+            for sequence in plot_data:
+                if sequence[-1].label == r[0].label and \
+                   sequence[-1].generation == g-1:
+                    sequence.append(r[1])
+                    appended.append(r[1].label)
+                    counter += 1
+                    # Do NOT break here as it's not necessarily 1:1
 
         # Handle entries that have no correspondence with previous generation
-        for c in [_ for _  in msm.mapping_.values() if _ not in appended]:
-            plot_data[g].append([ (msm.populations_[c], 100.0) ])
+        for c, l in [ x for x in msm.mapping_.items() if x[0] not in appended]:
+            plot_data.append([ Cluster(label=c,
+                                     generation=g,
+                                     rmsd_to_bound=np.mean(rmsd_to(boundcoords,
+                                                                   means[c]))
+                                                   if np.any(boundcoords)
+                                                   else None,
+                                     hub_score=scores[l],
+                                     population=msm.populations_[l])
+                             ])
             counter += 1
-        prevmsm = msm
 
-    # Trim out entries with no sequence (TODO)
-    print("should be %d is %d ideal %d" % (counter, sum(len(_) for p in plot_data.values() for _ in p),
+    # Trim out entries with no sequence
+    plot_data = [p for p in plot_data if len(p) > 1]
+    print("should be %d is %d ideal %d" % (counter,
+                                           sum(len(_) for _ in plot_data),
                                            (maxgen-mingen)*50))
     return plot_data
 
