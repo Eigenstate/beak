5c8e03e3cb7d816dd7866c43a45e11f72ed29eef
Robin Betz <robin@robinbetz.com>
2017-11-28 13:35:30 -0800

    Grid integrator now up and working. Can also generate subgrids that can be saved and visualized.

--- beak/analyze/sampling.py
+++ beak/analyze/sampling.py
@@ -2,55 +2,103 @@
 Methods to analyze the amount of sampling in a simulation
 """
 import numpy as np
+from gridData import Grid
 from vmd import atomsel
-
-#===============================================================================
-
-def round_to_half(n):
-    """
-    Rounds a number to the nearest grid spacing, which is on the 0.5
-    """
-    correction = 0.5 if n >=0 else -0.5
-    return int(n + correction) + correction
 
 #===============================================================================
 
 def get_box(residue, molid):
     """
-    Gets a box around a given residue. Box is sized for atom
-    center + 1/2 largest VDW radius.
+    Gets a box around a given residue, excluding hydrogens.
+    Box is sized for atom center + VDW radius, on a per-atom basis.
+    There's no need to round the box to the nearest grid spacing
+    as the integration function finds this.
 
     Args:
         residue (int): Residue number
         molid (int): VMD molecule ID
-        precision (float): Round to nearest THIS
 
     Returns:
-        [[xmin, xmax], [ymin, ymax], [zmin, zmax]]
+        (list of float): [[xmin, xmax], [ymin, ymax], [zmin, zmax]]
     """
-    sel = atomsel("residue %d" % residue, molid)
-    radius = max(sel.get("radius"))
+    sel = atomsel("noh and residue %d" % residue, molid)
+    radius = np.asarray(sel.get("radius"))
     result = []
     for coordinate in ["x", "y", "z"]:
-        minval = min(sel.get(coordinate)) + radius
-        maxval = max(sel.get(coordinate)) + radius
-        result.append([round_to_half(minval),
-                       round_to_half(maxval)])
+        minval = np.min(np.asarray(sel.get(coordinate)) - radius)
+        maxval = np.max(np.asarray(sel.get(coordinate)) + radius)
+        result.append([minval, maxval])
 
     return result
 
 #===============================================================================
 
-def integrate(residue, molid, grid):
+def get_indices(residue, molid, density):
     """
-    Sums the values of the grid covering the given residue
-    """
+    Gets indices into grid that match coordinates of given residue.
 
+    Args:
+        residue (int): Residue number
+        molid (int): VMD molecule ID
+        density (Grid): Density map to index into
+
+    Returns:
+        (list of int): [[xmin, xmax], [ymin, ymax], [zmin,zmax]]
+    """
     indices = []
     for aid, axis in enumerate(get_box(residue, molid)):
-        minval = np.min(np.where(grid.edges[aid] > axis[0])) - 1
-        maxval = np.max(np.where(grid.edges[aid] < axis[1])) + 1
+        minval = np.min(np.where(density.edges[aid] > axis[0])) - 1
+        maxval = np.max(np.where(density.edges[aid] < axis[1])) + 1
         indices.append([minval, maxval])
-
     return indices
+
+#===============================================================================
+
+def integrate(residue, molid, density):
+    """
+    Sums the values of the grid covering the given residue.
+    First finds the grid indices corresponding to the residue box,
+    erring on the side of a larger box (minimum box enclosing residue).
+
+    Args:
+        residue (int): Residue number
+        molid (int): VMD molecule ID
+        density (Grid): gridData Grid containing ligand density to integrate
+
+    Returns:
+        (float): Sum of grid squares residsue covers
+    """
+
+    indices = get_indices(residue, molid, density)
+    # Use numpy fancy indexing to get a view into the grid
+    total = np.sum(density.grid[indices[0][0]:indices[0][1],
+                                indices[1][0]:indices[1][1],
+                                indices[2][0]:indices[2][1]])
+
+    return total
+
+#===============================================================================
+
+def get_subgrid(residue, molid, density):
+    """
+    Returns a subgrid centered around the given residue. No interpolation
+    is performed
+
+    Args:
+        residue (int): Residue number
+        molid (int):  VMD molecule ID
+        density (Grid): Grid to subsample
+
+    Returns:
+        (Grid): Subgrid covered by residue
+    """
+    indices = get_indices(residue, molid, density)
+    # Use numpy fancy indexing to get a view into the grid
+    subgrid = density.grid[indices[0][0]:indices[0][1],
+                           indices[1][0]:indices[1][1],
+                           indices[2][0]:indices[2][1]]
+    edges = [density.edges[i][e[0]:e[1]] for i, e in enumerate(indices)]
+
+    return Grid(grid=subgrid, edges=edges)
+
 #===============================================================================
