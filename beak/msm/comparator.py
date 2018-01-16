7668103b70913717296b7a321340e3ed4e87aff6
Robin Betz <robin@robinbetz.com>
2018-01-16 13:26:42 -0800

    Add trypsin and full plot support to comparator

--- beak/msm/comparator.py
+++ beak/msm/comparator.py
@@ -7,6 +7,7 @@ import numpy as np
 
 from beak.msm import utils
 from configparser import ConfigParser
+from plotly.graph_objs import Figure, Layout, Scatter
 from vmd import molecule, vmdnumpy
 
 #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
@@ -30,6 +31,15 @@ class Cluster(object):
         self.rmsd_to_bound = kwargs.get("rmsd_to_bound")
         self.hub_score = kwargs.get("hub_score")
         self.population = kwargs.get("population")
+        self.variance = kwargs.get("variance")
+
+    #===========================================================================
+
+    def get_label(self):
+        """
+        Returns generation and label separated by spaces
+        """
+        return "%s %s" % (self.generation, self.label)
 
     #===========================================================================
 
@@ -40,7 +50,7 @@ class Cluster(object):
 
 def get_bound_state(rootdir):
     """
-    Returns bound coordinates as numpy array. Only b2ar supported
+    Returns bound coordinates as numpy array. Only b2ar and trypsin supported
     """
     mid = None
     if "B2AR" in rootdir and "50ns" in rootdir:
@@ -51,6 +61,15 @@ def get_bound_state(rootdir):
                                                 "prep", "4RWD_prepped_bound.mae"))
         mask = vmdnumpy.atomselect(mid, 0,
                                    "noh and same fragment as resname DI7 DI78")
+    elif "B2AR" in rootdir and "DESRES" in rootdir:
+        mid = molecule.load("mae", os.path.join(rootdir, "..",
+                                                "bound_5_aligned.mae"))
+        mask = vmdnumpy.atomselect(mid, 0, "noh and resname DALP")
+    elif "trypsin" in rootdir:
+        basedir = os.path.join(rootdir, "..", "..", "prep")
+        mid = molecule.load("psf", os.path.join(basedir, "inp08_3PTB_1lig_lesswat.psf"),
+                            "pdb", os.path.join(basedir, "inp08_3PTB_1lig_lesswat.pdb"))
+        mask = vmdnumpy.atomselect(mid, 0, "noh and resname BAMI")
 
     if mid:
         coords = np.compress(mask, vmdnumpy.timestep(mid, 0), axis=0)
@@ -70,7 +89,8 @@ def rmsd_to(coords, boundcoords):
 #==============================================================================
 
 def compare_clusters(gen, prevgen, rootdir,
-                     varthreshold=5.0, threshold=5.0, **kwargs):
+                     varthreshold=5.0, threshold=5.0,
+                     varmax=10.0, **kwargs):
     """
     Finds clusters that match up in terms of mean position.
 
@@ -80,6 +100,7 @@ def compare_clusters(gen, prevgen, rootdir,
         rootdir (str): Root simulation directory
         varthreshold (float): Variance thereshold factor (TODO)
         threshold (float): RMSD threshold factor (TODO)
+        varmax (float): Maximum allowable variance in a clustesr
 
     Returns:
         (list of tuple of Clusters): Matching clusters, inorder
@@ -102,11 +123,10 @@ def compare_clusters(gen, prevgen, rootdir,
     oldmsm = utils.load(os.path.join(d, "mmsm_G%d.pkl" % prevgen))
     oldscores = utils.load(os.path.join(d, "mmsm_scores.pkl"))
 
-    if len(varxs.keys()) != 50:
-        print("MISSING KEYS: gen %d" % gen)
-
-    validmeans = [m for m in means if np.mean(varxs[m]) < varthreshold]
-    validoldmeans = [m for m in oldmeans if np.mean(oldvarxs[m]) < varthreshold]
+    validmeans = [m for m in means if np.mean(varxs[m]) < varthreshold
+                  and np.max(varxs[m]) < varmax]
+    validoldmeans = [m for m in oldmeans if np.mean(oldvarxs[m]) < varthreshold
+                     and np.max(oldvarxs[m]) < varmax]
 
     # Get + load bound state, solvent population
     boundcoords = get_bound_state(rootdir)
@@ -128,7 +148,8 @@ def compare_clusters(gen, prevgen, rootdir,
                              rmsd_to_bound=np.mean(rmsd_to(oldmeans[mm],
                                                            boundcoords))
                                            if np.any(boundcoords)
-                                           else None)
+                                           else None,
+                             variance=np.mean(oldvarxs[mm]))
                 c2 = Cluster(label=m,
                              generation=gen,
                              population=msm.populations_[l2],
@@ -136,7 +157,8 @@ def compare_clusters(gen, prevgen, rootdir,
                              rmsd_to_bound=np.mean(rmsd_to(means[m],
                                                            boundcoords))
                                            if np.any(boundcoords)
-                                           else None)
+                                           else None,
+                             variance=np.mean(varxs[m]))
 
                 candidate = (c1, c2)
                 bestrmsd = rmsd
@@ -148,12 +170,15 @@ def compare_clusters(gen, prevgen, rootdir,
 
 #==============================================================================
 
-def plot_cluster_progress(config, mingen, maxgen):
+def get_cluster_progress(config, mingen, maxgen, filter=True):
     """
     Plots the cluster progress, using all available data
 
     Args:
         config (str): Path to configuration file for run
+        mingen (int): First generation to plot
+        maxgen (int): Last generation to plot
+        filter (bool): Only show clusters present in most recent generation
 
     Returns:
         (list of lists of Clusters) : Equivalent clusters
@@ -161,6 +186,10 @@ def plot_cluster_progress(config, mingen, maxgen):
     assert os.path.isfile(config)
     cfg = ConfigParser(interpolation=None)
     cfg.read(config)
+    if os.environ.get("SHERLOCK") is None:
+        for var in cfg["system"]:
+            cfg["system"]["var"].replace("/scratch/PI/rondror/",
+                                        "/mnt/sherlock/scratch/")
     rootdir = cfg.get("system", "rootdir")
     boundcoords = get_bound_state(rootdir)
 
@@ -189,7 +218,9 @@ def plot_cluster_progress(config, mingen, maxgen):
                     # Do NOT break here as it's not necessarily 1:1
 
         # Handle entries that have no correspondence with previous generation
-        for c, l in [ x for x in msm.mapping_.items() if x[0] not in appended]:
+        for c, l in [ x for x in msm.mapping_.items()
+                     if (x[0] not in appended)
+                     and (means.get(x[0]) is not None) ]:
             plot_data.append([ Cluster(label=c,
                                      generation=g,
                                      rmsd_to_bound=np.mean(rmsd_to(boundcoords,
@@ -203,9 +234,47 @@ def plot_cluster_progress(config, mingen, maxgen):
 
     # Trim out entries with no sequence
     plot_data = [p for p in plot_data if len(p) > 1]
-    print("should be %d is %d ideal %d" % (counter,
-                                           sum(len(_) for _ in plot_data),
-                                           (maxgen-mingen)*50))
+
+    # Filter entries not in maxgen if desired
+    if filter:
+        plot_data = [p for p in plot_data
+                     if max(d.generation for d in p) >= maxgen]
     return plot_data
+
+#==============================================================================
+
+def plot_cluster_progress(config, mingen, maxgen, filter=True):
+    """
+    Returns a graph of cluster progress.
+
+    Args:
+        config (str): Path to configuration file for run
+        mingen (int): First generation to plot
+        maxgen (int): Last generation to plot
+        filter (bool): Only show clusters present in most recent generation
+    """
+    plotme = []
+    x = get_cluster_progress(config, mingen, maxgen, filter=True)
+    first = True
+    for d in x:
+        if filter and maxgen not in [x.generation for x in d]: continue # Show only current traces
+        plotme.append(Scatter(x=[x.generation for x in d],
+                              y=[x.population for x in d],
+                              marker=dict(
+                                  color=[x.rmsd_to_bound for x in d],
+                                  colorscale="Portland",
+                                  showscale=True if first else False,
+                                  cmax=10.0,
+                                  cmin=0.0,
+                                  size=9,
+                              ),
+                              text=[str(x) for x in d],
+                              line=dict(color="black"),
+                              mode="lines+markers"))
+        first = False
+        layout = Layout(yaxis=dict(range=[0,0.01]),
+                        xaxis=dict(range=[mingen, maxgen]),
+                        showlegend=False)
+    return Figure(Data=plotme, layout=layout)
 
 #==============================================================================
