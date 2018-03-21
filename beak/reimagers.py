2a0b6851756833f8bb2e6e2936e51256f6bff3dc
Robin Betz <robin@robinbetz.com>
2018-03-21 13:14:32 -0700

    Add --mdstep flag to reimage to allow reimaging of mdstep-style trajectories. Also refactor regular reimaging a bit so it should detect and remove old reimaged trajectories easily.

--- beak/reimagers.py
+++ beak/reimagers.py
@@ -100,13 +100,14 @@ def get_protein_residues(topology):
 
 #==============================================================================
 
-def reimage(psf, revision, skip, alleq, align, stripmask=None):
+def reimage(basedir, psf, revision, skip, alleq, align, stripmask=None):
     """
     Reimages all Prod_[0-9]*.nc files in a given directory
     into one Prod_all_reimaged.nc file, with the protein at
     the center
 
     Args:
+        basedir (str): Base directory to find all replicates
         psf (str): Path to the psf file, used to identify protein
         revision (str): The revision to reimage
         skip (int): Offset
@@ -119,6 +120,7 @@ def reimage(psf, revision, skip, alleq, align, stripmask=None):
         IOError if the replicate directory is not present or empty
         ValueError if the cpptraj call fails
     """
+    os.chdir(basedir)
     revision = str(revision)
     # Error checking
     if not os.path.isfile(psf):
@@ -154,6 +156,7 @@ def reimage_single_dir(topology, replicate, revision, skip, alleq, align,
     prods = sorted(glob(os.path.join(proddir, "Prod_[0-9]*.nc")),
                    key=lambda x: int(x.replace(os.path.join(proddir, "Prod_"),
                                                "").replace(".nc", "")))
+    prods = [x for x in prods if "Reimaged" not in x]
 
     if check_empty(os.path.join(proddir, "Eq_6.nc")):
         print("NO production simulation in Rev %s Rep %s" % (revision, replicate))
@@ -166,33 +169,22 @@ def reimage_single_dir(topology, replicate, revision, skip, alleq, align,
     else:
         lastnum = "Eq6"
 
-    # If output file already exists, continue
-    if alleq:
-        ofile = os.path.join(proddir, "Reimaged_Eq1_to_%s_skip_%s.nc" % (lastnum, skip))
-        ofile2 = os.path.join(proddir, "Reimaged_strip_Eq1_to_%s_skip_%s.nc" % (lastnum, skip))
-    else:
-        ofile = os.path.join(proddir, "Reimaged_Eq6_to_%s_skip_%s.nc" % (lastnum, skip))
-        ofile2 = os.path.join(proddir, "Reimaged_strip_Eq6_to_%s_skip_%s.nc" % (lastnum, skip))
-    if os.path.isfile(ofile) and os.path.isfile(ofile2):
-        print("EXISTS reimaged file for Rev %s Rep %s" % (revision, replicate))
-        return
-    else:
-        if alleq:
-            if stripmask:
-                rems = glob(os.path.join("production", revision, replicate,
-                                         "Reimaged_strip_Eq1_to_*_skip_%s.nc" % skip))
-            else:
-                rems = glob(os.path.join("production", revision, replicate,
-                                         "Reimaged_Eq1_to_*_skip_%s.nc" % skip))
-        else:
-            rems = glob(os.path.join("production", revision, replicate,
-                                     "Reimaged_Eq6_to_*_skip_%s.nc" % skip))
-        # Delete reimaged files that are older than last modified production trajectory
-        for r in rems:
-            if os.path.getmtime(r) > os.path.getmtime(prods[-1]):
-                print("Removing: %s" % r)
-                sys.stdout.flush()
-                os.remove(r)
+    # Set output file names
+    rprefix = "Reimaged_strip" if stripmask else "Reimaged_"
+    ofile = os.path.join(proddir, "%s_Eq%d_to_%s_skip_%s.nc"
+                         % (rprefix, 1 if alleq else 6,
+                            lastnum, skip))
+
+    # If reimaged output exists, only continue if latest production
+    # file has been updated since then. Delete all matching reimaged
+    # files that are older so directories aren't cluttered up.
+    rems = glob(os.path.join(proddir, "%s_Eq%d_to_*_skip_%s.nc"
+                             % (rprefix, 1 if alleq else 6, skip)))
+    for r in rems:
+        if os.path.getmtime(r) > os.path.getmtime(prods[-1]):
+            print("Removing: %s" % r)
+            sys.stdout.flush()
+            os.remove(r)
 
     # Now write cpptraj input
     tempfile = open(os.path.join(proddir, "tempfile"), 'w')
@@ -218,11 +210,8 @@ def reimage_single_dir(topology, replicate, revision, skip, alleq, align,
                                               int(skip)*8))
 
     # Read in production data, reimaged
-    for p in prods:
-        if "Reimaged" in p:
-            continue
-        if not check_empty(p):
-            tempfile.write("trajin %s\n" % p)
+    for p in [x for x in prods if not check_empty(x)]:
+        tempfile.write("trajin %s\n" % p)
 
     protein_residues = get_protein_residues(topology)
 
@@ -233,7 +222,6 @@ def reimage_single_dir(topology, replicate, revision, skip, alleq, align,
         tempfile.write("rms toRef ref [ref] @CA\n")
 
     if stripmask is not None:
-        ofile = ofile.replace("Reimaged_", "Reimaged_strip_")
         tempfile.write("strip (%s) parmout %s_stripped.prmtop\n"
                        % (stripmask, topoprefix))
     tempfile.write("trajout %s offset %s\n" % (ofile, skip))
@@ -247,3 +235,134 @@ def reimage_single_dir(topology, replicate, revision, skip, alleq, align,
 
 #==============================================================================
 
+def reimage_single_mdstep(topology, basedir, skip, alleq,
+                          align, stripmask=None):
+    """
+    Reimages a trajectory that is contained entirely in one directory
+    with the "system.psf" naming convention associated with mdstep runs.
+
+    Args:
+        topology (str): Topology filename
+        basedir (str): Directory containing all files
+        skip (int): Stride for outputting frames
+        alleq (bool): True to include all the equilibration, too
+        align (bool): True to align all frames to first one
+        stripmask (str): Amber-style atom selection of atoms to omit
+            from the reimaged trajectory
+
+    Returns:
+        0 on success
+    """
+    # Enumerate production files
+    prods = sorted(glob(os.path.join(basedir, "Prod_[0-9]*.nc")),
+                   key=lambda x: int(x.replace(os.path.join(basedir, "Prod_"),
+                                               "").replace(".nc", "")))
+    prods = [x for x in prods if "Reimaged" not in x]
+
+    if check_empty(os.path.join(basedir, "Eq_6.nc")):
+        print("No production simulation in %s" % basedir)
+        return None
+
+    # Get number of last production file
+    if len(prods):
+        lastnum = prods[-1].replace(os.path.join(basedir,
+                                                 "Prod_"), "").replace(".nc", "")
+    else:
+        lastnum = "Eq6"
+
+    # Set output file names
+    rprefix = "Reimaged_strip" if stripmask else "Reimaged_"
+    ofile = os.path.join(basedir, "%s_Eq%d_to_%s_skip_%s.nc"
+                         % (rprefix, 1 if alleq else 6,
+                            lastnum, skip))
+
+    # If reimaged output exists, only continue if latest production
+    # file has been updated since then. Delete all matching reimaged
+    # files that are older so directories aren't cluttered up.
+    rems = glob(os.path.join(basedir, "%s_Eq%d_to_*_skip_%s.nc"
+                             % (rprefix, 1 if alleq else 6, skip)))
+    for r in rems:
+        if os.path.getmtime(r) > os.path.getmtime(prods[-1]):
+            print("Removing: %s" % r)
+            sys.stdout.flush()
+            os.remove(r)
+
+    # Now write cpptraj input
+    tempfile = open(os.path.join(basedir, "tempfile"), 'w')
+
+    topoprefix = ".".join(topology.split(".")[:-1])
+    if align:
+        tempfile.write("reference %s.inpcrd parm %s [ref]\n"
+                       % (topoprefix, topology))
+
+    # equilibration written 8x more frequently so downsample
+    if alleq:
+        eqs = sorted(glob(os.path.join(basedir, "Eq_[0-5]*.nc")),
+                     key=lambda x: int(x.replace(os.path.join(basedir, "Eq_"),
+                                                 "").replace(".nc", "")))
+        for e in eqs:
+            tempfile.write("trajin %s 1 last %d\n" % (e, int(skip)*8))
+
+    # Last equilibration in
+    tempfile.write("trajin %s 1 last %d\n" % (os.path.join(basedir, "Eq_6.nc"),
+                                              int(skip)*8))
+
+    # Read in production data, reimaged
+    for p in [x for x in prods if not check_empty(x)]:
+        tempfile.write("trajin %s\n" % p)
+
+    protein_residues = get_protein_residues(topology)
+
+    tempfile.write("center origin (:%s)\n" % protein_residues)
+    tempfile.write("image origin center\n")
+
+    if align:
+        tempfile.write("rms toRef ref [ref] @CA\n")
+
+    if stripmask is not None:
+        tempfile.write("strip (%s) parmout %s_stripped.prmtop\n"
+                       % (stripmask, topoprefix))
+    tempfile.write("trajout %s offset %s\n" % (ofile, skip))
+    tempfile.write("go\n")
+    tempfile.close()
+
+    # Returns 0 on success
+    return subprocess.call("%s/bin/cpptraj -p %s -i %s/tempfile" %
+                           (os.environ['AMBERHOME'], topology, basedir),
+                           shell=True)
+
+#==============================================================================
+
+def reimage_mdstep(basedir, skip, alleq, align, stripmask=None):
+    """
+    Reimages all subdirectories of this one, with mdstep conventions
+    for file naming.
+
+    Args:
+        basedir (str): Base directory to collect replicates from
+        skip (int): Offset
+        alleq (bool): Whether or not to include all equilibration setps
+        align (bool): Whether or not to also align the trajectory
+        stripmask (str): AMBER selection to remove from reimaged trajectory
+
+    Raises:
+        ValueError if no valid replicate directories with system.psf are found
+        ValueError if the cpptraj call fails
+    """
+    replicate_dirs = [d for d in os.listdir(basedir) \
+                      if os.path.isdir(d) and \
+                      os.path.isfile(os.path.join(d, "system.psf"))]
+
+    # Error checking
+    if not replicate_dirs:
+        raise ValueError("No replicate directories in '%s' "
+                         "Each replicate directory needs a system.psf to be "
+                         "present to be correctly identified")
+
+    p = Pool(int(os.environ.get("SLURM_NTASKS", "1")))
+    p.starmap(reimage_single_mdstep, [(os.path.join(d, "system.psf"), d,
+                                       skip, alleq, align, stripmask)
+                                      for d in replicate_dirs])
+    p.close()
+
+#==============================================================================
